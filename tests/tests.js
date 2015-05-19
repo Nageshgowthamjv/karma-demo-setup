b10c9791456e510e8dc3f093cb73adccd041b71a
nGowtham <nagesh.gowtham@gmail.com>
2015-05-19 10:58:01 +0530

    * adding Demo setup

---
+++ tests/tests.js
@@ -0,0 +1,27 @@
+// tests.js
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      chai.assert.equal(-1, [1,2,3].indexOf(5));
+      chai.assert.equal(-1, [1,2,3].indexOf(6));
+    });
+  });
+});
+
+describe('Array', function(){
+  describe('#indexOf1()', function(){
+    it('should return -12 when the value is not present', function(){
+      chai.assert.equal(-1, [1,2,3].indexOf(5));
+      chai.assert.equal(-1, [1,2,3].indexOf(6));
+    });
+  });
+});
+
+describe('Array', function(){
+  describe('#indexOf2()', function(){
+    it('should return -13 when the value is not present', function(){
+      chai.assert.equal(-1, [1,2,3].indexOf(5));
+      chai.assert.equal(-1, [1,2,3].indexOf(6));
+    });
+  });
+});
